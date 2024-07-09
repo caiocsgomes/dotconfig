51517a76a7daab68086f1cc954b7cc79fdf7007a
Caio Gomes <caio.gomes@transferwise.com>
2024-07-09 10:04:15 +0300

    Adding unfinished work

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -11,7 +11,12 @@ return {
 				"bashls",
 				"cssls",
 				"dockerls",
+				"delve",
 				"gopls",
+				"goimports",
+				"gofumpt",
+				"gomodifytags",
+				"impl",
 				"html",
 				"jsonls",
 				"pyright",
@@ -23,9 +28,9 @@ return {
 		},
 	},
 	{
-		-- Troubleshooting: LspInfo
+		-- Troubleshooting: LspInfo, LspLog
 		"neovim/nvim-lspconfig",
-    lazy = false,
+		lazy = false,
 		dependencies = {
 			"mason.nvim",
 			{ "williamboman/mason-lspconfig.nvim", config = function() end },
@@ -40,6 +45,70 @@ return {
 				-- Your COQ settings here
 			}
 		end,
+		opts = {
+			servers = {
+				gopls = {
+					settings = {
+						gopls = {
+							gofumpt = true,
+							codelenses = {
+								gc_details = false,
+								generate = true,
+								regenerate_cgo = true,
+								run_govulncheck = true,
+								test = true,
+								tidy = true,
+								upgrade_dependency = true,
+								vendor = true,
+							},
+							hints = {
+								assignVariableTypes = true,
+								compositeLiteralFields = true,
+								compositeLiteralTypes = true,
+								constantValues = true,
+								functionTypeParameters = true,
+								parameterNames = true,
+								rangeVariableTypes = true,
+							},
+							analyses = {
+								fieldalignment = true,
+								nilness = true,
+								unusedparams = true,
+								unusedwrite = true,
+								useany = true,
+							},
+							usePlaceholders = true,
+							completeUnimported = true,
+							staticcheck = true,
+							directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+							semanticTokens = true,
+						},
+					},
+				},
+			},
+			setup = {
+				gopls = function(_, opts)
+					-- workaround for gopls not supporting semanticTokensProvider
+					-- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+					local lspconfig = require("lspconfig")
+					local lsp_defaults = lspconfig.util.default_config
+					lsp_defaults.lsp.on_attach(function(client, _)
+						if not client.server_capabilities.semanticTokensProvider then
+							local semantic = client.config.capabilities.textDocument.semanticTokens
+							client.server_capabilities.semanticTokensProvider = {
+								full = true,
+								legend = {
+									tokenTypes = semantic.tokenTypes,
+									tokenModifiers = semantic.tokenModifiers,
+								},
+								range = true,
+							}
+						end
+					end, "gopls")
+					-- end workaround
+				end,
+			},
+		},
 		config = function()
 			local lspconfig = require("lspconfig")
 			local lsp_defaults = lspconfig.util.default_config
