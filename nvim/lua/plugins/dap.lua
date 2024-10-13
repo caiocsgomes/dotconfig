b01653ff61e4ecee51971c57473a7faacf5abecb
caiocsgomes <caiocsgomes@gmail.com>
2024-10-13 22:00:54 +0300

    chore: updating config

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -193,6 +193,37 @@ return {
         },
       },
     }
+    dap.adapters["local-lua"] = {
+      type = "executable",
+      command = "node",
+      args = {
+        os.getenv('HOME') .. '/local-lua-debugger-vscode/extension/debugAdapter.js'
+      },
+      enrich_config = function(config, on_config)
+        if not config["extensionPath"] then
+          local c = vim.deepcopy(config)
+          -- 💀 If this is missing or wrong you'll see
+          -- "module 'lldebugger' not found" errors in the dap-repl when trying to launch a debug session
+          c.extensionPath = os.getenv('HOME') .. '/local-lua-debugger-vscode/'
+          on_config(c)
+        else
+          on_config(config)
+        end
+      end,
+    }
+    dap.configurations.lua = {
+      {
+        name = 'Current file (local-lua-dbg, lua)',
+        type = 'local-lua',
+        request = 'launch',
+        cwd = '${workspaceFolder}',
+        program = {
+          lua = 'lua5.4',
+          file = '${file}',
+        },
+        args = {},
+      },
+    }
     -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
     -- dap.configurations.go = {
     -- 	{
