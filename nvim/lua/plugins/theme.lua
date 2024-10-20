6a915d66cf8d893c551ffbdf13e81518277be2b5
Caio Gomes <caio.gomes@transferwise.com>
2024-10-20 12:04:53 +0300

    chore: updating config

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,53 +1,64 @@
 -- https://nvchad.com/themes/
 return {
-  {
-    "ellisonleao/gruvbox.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("gruvbox").setup({
-        terminal_colors = true, -- add neovim terminal colors
-        undercurl = true,
-        underline = true,
-        bold = true,
-        italic = {
-          strings = true,
-          emphasis = true,
-          comments = true,
-          operators = false,
-          folds = true,
-        },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        invert_intend_guides = false,
-        inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = "", -- can be "hard", "soft" or empty string
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = false,
-      })
-      vim.cmd("colorscheme gruvbox")
-    end,
-  },
-  -- {
-  -- 	"folke/tokyonight.nvim",
-  -- 	lazy = false,
-  -- 	priority = 1000,
-  -- 	opts = {},
-  -- 	config = function()
-  -- 		require("tokyonight").setup()
-  -- 		vim.cmd("colorscheme tokyonight")
-  -- 	end,
-  -- },
-  --   {
-  --     'AlexvZyl/nordic.nvim',
-  --     lazy = false,
-  --     priority = 1000,
-  --     config = function()
-  --         require 'nordic' .load()
-  --     end
-  -- }
+	-- {
+	-- 	"ellisonleao/gruvbox.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		require("gruvbox").setup({
+	-- 			terminal_colors = true, -- add neovim terminal colors
+	-- 			undercurl = true,
+	-- 			underline = true,
+	-- 			bold = true,
+	-- 			italic = {
+	-- 				strings = true,
+	-- 				emphasis = true,
+	-- 				comments = true,
+	-- 				operators = false,
+	-- 				folds = true,
+	-- 			},
+	-- 			strikethrough = true,
+	-- 			invert_selection = false,
+	-- 			invert_signs = false,
+	-- 			invert_tabline = false,
+	-- 			invert_intend_guides = false,
+	-- 			inverse = true, -- invert background for search, diffs, statuslines and errors
+	-- 			contrast = "", -- can be "hard", "soft" or empty string
+	-- 			palette_overrides = {},
+	-- 			overrides = {},
+	-- 			dim_inactive = false,
+	-- 			transparent_mode = false,
+	-- 		})
+	-- 		vim.cmd("colorscheme gruvbox")
+	-- 	end,
+	-- },
+	{
+		"sainnhe/gruvbox-material",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- Optionally configure and load the colorscheme
+			-- directly inside the plugin declaration.
+			vim.g.gruvbox_material_enable_italic = true
+			vim.cmd.colorscheme("gruvbox-material")
+		end,
+	},
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	opts = {},
+	-- 	config = function()
+	-- 		require("tokyonight").setup()
+	-- 		vim.cmd("colorscheme tokyonight")
+	-- 	end,
+	-- },
+	--   {
+	--     'AlexvZyl/nordic.nvim',
+	--     lazy = false,
+	--     priority = 1000,
+	--     config = function()
+	--         require 'nordic' .load()
+	--     end
+	-- }
 }
