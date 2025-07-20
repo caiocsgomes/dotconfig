a29d348dbbbc339f41cf0f5da7cd0b3ca5a7625a
Caio Gomes <caio.gomes@transferwise.com>
2025-07-20 10:12:51 +0300

    chore: updating config

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -28,6 +28,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.coding.blink" },
+    { import = "lazyvim.plugins.extras.ai.copilot" },
 
     -- import/override with your plugins
     { import = "plugins" },
--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,36 +1,25 @@
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  build = ":Copilot auth",
-  event = "BufReadPost",
-  keys = {
-    {
-      "<leader>at",
-      function()
-        if require("copilot.client").is_disabled() then
-          require("copilot.command").enable()
-        else
-          require("copilot.command").disable()
-        end
-      end,
-      desc = "Toggle (Copilot)",
-    },
-  },
-  opts = {
-    suggestion = {
-      -- enabled = not vim.g.ai_cmp,
-      auto_trigger = true,
-      hide_during_completion = vim.g.ai_cmp,
-      keymap = {
-        accept = "<Tab>", -- handled by nvim-cmp / blink.cmp
-        next = "<M-]>",
-        prev = "<M-[>",
+  {
+    "zbirenbaum/copilot.lua",
+    keys = {
+      {
+        "<leader>at",
+        function()
+          if require("copilot.client").is_disabled() then
+            require("copilot.command").enable()
+          else
+            require("copilot.command").disable()
+          end
+        end,
+        desc = "Toggle (Copilot)",
       },
     },
-    panel = { enabled = false },
-    filetypes = {
-      markdown = true,
-      help = true,
+    opts = {
+      suggestion = {
+        keymap = {
+          acceptword = "<C-[>",
+        },
+      },
     },
   },
 }
