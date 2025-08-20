c0760fc5a1f0c4934af994f2bf067806a36ae79d
caiocsgomes <caiocsgomes@gmail.com>
2025-08-20 22:11:07 +0300

    chore: updating config

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,16 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  config = function()
+    local actions = require("telescope.actions")
+    require("telescope").setup({
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-j>"] = actions.move_selection_next,
+          },
+        },
+      },
+    })
+  end,
+}
