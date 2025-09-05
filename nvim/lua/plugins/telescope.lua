ef130ad60a1723ef84099bc52eb5410d19c9c3b3
Caio Gomes <caio.gomes@transferwise.com>
2025-09-05 12:06:58 +0300

    chore: adding telescope config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,6 +8,19 @@ return {
           ["<C-j>"] = require("telescope.actions").move_selection_next,
         },
       },
+      file_ignore_patterns = {},
+      hidden = true,
+    },
+    pickers = {
+      find_files = {
+        hidden = true,
+      },
+      grep_string = {
+        additional_args = { "--hidden" },
+      },
+      live_grep = {
+        additional_args = { "--hidden" },
+      },
     },
   },
 }
