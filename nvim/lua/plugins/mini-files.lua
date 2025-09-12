bc0302f7240b6af60c2d54104acd01a9e6575a7d
caiocsgomes <caiocsgomes@gmail.com>
2025-09-12 20:12:50 +0300

    chore: updating config

--- nvim/lua/plugins/mini-files.lua
+++ nvim/lua/plugins/mini-files.lua
@@ -4,19 +4,5 @@ return {
     windows = {
       preview = false,
     },
-    options = {
-      -- Whether to use for editing directories
-      -- Disabled by default in LazyVim because neo-tree is used for that
-      use_as_default_explorer = false,
-    },
-  },
-  keys = {
-    {
-      "<leader>m",
-      function()
-        require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
-      end,
-      desc = "Open mini.files (Directory of Current File)",
-    },
   },
 }
